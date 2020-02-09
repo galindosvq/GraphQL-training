5299f83cea9bf605989524cbb67bc89df09d5822
Antonio Galindo <galindosvq@gmail.com>
2020-02-10 00:47:09 +0100

    feat: created index server file

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const express_graphql = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+const root = {
+  message: () => "hello world!"
+};
+
+app.use(
+  "/graphql",
+  express_graphql({
+    schema: schema,
+    root: root,
+    graphiql: true
+  })
+);
+app.listen(3000, () => console.log("server is running on port 3000"));
