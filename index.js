d3d5b33c973e246e0eab1b75029facd8d33e7689
Antonio Galindo <galindosvq@gmail.com>
2020-02-10 01:05:57 +0100

    feat: added query

--- index.js
+++ index.js
@@ -3,21 +3,50 @@ const app = express();
 const express_graphql = require("express-graphql");
 const { buildSchema } = require("graphql");
 
+const { courses } = require("./data.json");
+
 const schema = buildSchema(`
     type Query {
         message: String
+        course(id: Int!): Course
+        courses(topic: String!): [Course]
+    }
+    type Course {
+        id: Int
+        title: String
+        author: String
+        topic: String
+        url: String
     }
 `);
 
+const getCourse = args => {
+  const id = args.id;
+  return courses.filter(course => {
+    return course.id === id;
+  })[0];
+};
+
+const getCourses = args => {
+  if (args.topic) {
+    const topic = args.topic;
+    return courses.filter(course => course.topic === topic);
+  } else {
+    return courses;
+  }
+};
+
 const root = {
-  message: () => "hello world!"
+  message: () => "hello world!",
+  course: getCourse,
+  courses: getCourses
 };
 
 app.use(
   "/graphql",
   express_graphql({
     schema: schema,
-    root: root,
+    rootValue: root,
     graphiql: true
   })
 );
